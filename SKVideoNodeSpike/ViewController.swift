63c4ddf9f10d519e7cb17c4e3097ee53c0190561
Dexter Juntilla <dexter@dnamicro.com>
2018-07-05 16:36:31 +0800

    initial

--- SKVideoNodeSpike/ViewController.swift
+++ SKVideoNodeSpike/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import SpriteKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var playerItem:AVPlayerItem?
+    var player:AVPlayer?
+    var videoNode:SKVideoNode?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        let view = self.view as! SKView
+        
+        let scene = SKScene(size: self.view.frame.size)
+        view.presentScene(scene)
+        
+        
+        let url = URL(string: "https://www.youtube.com/watch?v=d1JA-nh0IfI")!
 
+        let asset = AVURLAsset.init(url: url)
+        
+        playerItem = AVPlayerItem(asset: asset)
+        player = AVPlayer(playerItem: playerItem)
+        
+        videoNode = SKVideoNode.init(avPlayer: player!)
+        
+//        let url = Bundle.main.url(forResource: "sample", withExtension: "mp4")!
+//        videoNode = SKVideoNode.init(url: url)
+        
+        videoNode?.position = CGPoint(x: scene.size.width/2, y: scene.size.height/2)
+        
+        scene.addChild(videoNode!)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        videoNode?.play()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
